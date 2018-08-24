d616d466a6de1cc83346e9328132693df6a0154d
MasaYan24 <m.takahashi.public+github@gmail.com>
2018-08-24 09:01:56 +0900

    complete

--- plot_bin.py
+++ plot_bin.py
@@ -3,26 +3,27 @@
 import argparse
 import pandas
 import matplotlib.pyplot as plt
+import matplotlib
+import subprocess
 import sys
 
 class ReadPlot(object):
-    def __init__(self,datafile="score.csv",figfile="score.png",cols=[0],sep='\t',trim=True):
+    def __init__(self,datafile="score.csv",figfile="score.png",cols=[0],sep='\t',trim=True,labels=None):
         self.__datafile = datafile
         self.__figfile = figfile
         self.__readfile(datafile=datafile,columns=cols)
         self.__trim = trim
+        if labels is not None:
+            self.__labels = labels
 
     def __readfile(self,datafile="score.csv",columns=[0]):
         self.__columns = columns
         self.__data = pandas.read_csv(datafile,header=None,encoding='utf-8',skipinitialspace=True,sep='\t',usecols=columns)
         self.__data.columns = [i for i in range(len(self.__data.columns))] # rename columns as sequence number
         self.__average = self.__data.mean()
-        print(self.__average)
-        print(self.__data[0].values)
         if self.__trim:
             for i in self.__data.columns:
                 self.__data[i] = self.__data[i].apply(self.__trim)
-                print(self.__data[i])
 
     def __trim(self,_x):
         if _x < 0: return 0
@@ -32,38 +33,39 @@ class ReadPlot(object):
 
 
     def plot(self):
+        font = {'family': 'Meiryo'}
+        matplotlib.rc('font', **font)
+        cmap = plt.get_cmap("tab10")
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
 
         ax.set_xlabel('score')
         ax.set_ylabel('number of people')
+        ax.set_xlim([0,100])
         ax.hist(self.__data.values,range=(0,100),rwidth=1.0)
         for i in self.__data.columns:
-            ax.axvline(self.__average[i],color='C'+str(i))
-        plt.show()
-#            print(self.__data[i])
-#        print(self.__data)
-#        self.__data_array = []
-#        print(self.__data[columns[0]])
-#        for i in range(len(columns)):
-#            print(self.__data[columns[i]])
-#            self.__data_array.append(self.__data[columns[i]])
-#        print(self.__data_array)
-#        for i in range(len(columns)):
-#            print(self.__data_array[i])
+            ax.axvline(self.__average[i],color=cmap(i),alpha=0.5)
+        try:
+            plt.legend(self.__labels)
+        except:
+            pass
+#        plt.show()
+        plt.savefig(self.__figfile)
+        subprocess.Popen("open "+self.__figfile,shell=True)
 
 
 if __name__ == '__main__':
     class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,argparse.MetavarTypeHelpFormatter):
         pass
-    parser = argparse.ArgumentParser(description="Plot score.", formatter_class=CustomFormatter)
+    parser = argparse.ArgumentParser(description="Plot score.",epilog="example: ./plot_bin.py --columns 0 2 --labels \"テスト\" \"総合\"", formatter_class=CustomFormatter)
     parser.add_argument('--file', '--datafile', type=str, dest='datafile', nargs=1, default='score.csv', help="input file")
-    parser.add_argument('--fig', type=str, dest='figfile', nargs=1, default='score.png', help="output figure file (png)")
-    parser.add_argument('--columns', type=int, dest='columns', nargs='+', default=[0], help="column of data")
+    parser.add_argument('--fig','--figfile', type=str, dest='figfile', nargs=1, default='score.png', help="output figure file (png)")
+    parser.add_argument('--columns','--cols', type=int, dest='columns', nargs='+', default=[0], help="column of data")
+    parser.add_argument('--labels', type=str, dest='labels', nargs='+', default=None, help="title column")
     parser.add_argument('--notrim', action="store_false", help="do not round-up/down score")
-    parser.add_argument('--sep', type=str, dest='sep', nargs=1, default='\t', help="specify data separater")
+    parser.add_argument('--sep', type=str, dest='sep', nargs=1, default='\t', help="specify data separater in datafile")
 
     args = parser.parse_args()
 
-    stat_inst = ReadPlot(datafile=args.datafile,figfile=args.figfile,cols=args.columns,sep=args.sep,trim=args.notrim)
+    stat_inst = ReadPlot(datafile=args.datafile,figfile=args.figfile,cols=args.columns,sep=args.sep,trim=args.notrim,labels=args.labels)
     stat_inst.plot()
